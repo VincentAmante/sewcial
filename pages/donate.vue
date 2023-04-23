ddda629bb7bf7d0d94872f0608a8bbd37821c0ab
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2023-04-23 20:18:06 +0400

    Restyled Donate
    - Set donate page to use tailwind stylings
    - Added better reponsivenss to Donate Page

--- pages/donate.vue
+++ pages/donate.vue
@@ -6,45 +6,60 @@ import TiltedHeading from '@/components/TiltedHeading.vue'
 <template>
   <main>
     <!-- Just test divs, don't mind -->
-    <section>
-      <div class="splash-headings">
+    <section class="flex flex-col items-center text-center text-secondary my-default-h gap-[4vw]">
+      <div class="text-6xl flex flex-col items-center justify-center text-secondary font-bold pt-5 gap-0 tablet:flex-row tablet:gap-4">
         <span>Support</span>
-        <TiltedHeading class="the">
+        <TiltedHeading>
           THE
         </TiltedHeading>
         <span>community</span>
       </div>
-      <div class="card-section">
-        <div class="info-card">
-          <img src="@/assets/icons/sharelove.png" alt="">
-          <h2>Share Love</h2>
-          <p>Drop off your pre-loved clothes at one of our stores</p>
+      <div class="flex flex-col tablet:flex-row gap-[6vw]">
+        <div class="info-card flex flex-col items-center text-center">
+          <img
+            src="@/assets/icons/sharelove.png"
+            alt=""
+          >
+          <h2 class="m-0">
+            Share Love
+          </h2>
+          <p class="m-0 max-w-[12rem] text-black">
+            Drop off your pre-loved clothes at one of our stores
+          </p>
         </div>
-        <div class="info-card">
+        <div class="flex flex-col items-center text-center">
           <img src="@/assets/icons/getrewards.png" alt="">
-          <h2>Get Rewards</h2>
-          <p>Receive a store credit voucher worth 10 AED</p>
+          <h2 class="m-0">
+            Get Rewards
+          </h2>
+          <p class="m-0 max-w-[12rem] text-black">
+            Receive a store credit voucher worth 10 AED
+          </p>
         </div>
-        <div class="info-card">
+        <div class="flex flex-col items-center text-center">
           <img src="@/assets/icons/star.png" alt="">
-          <h2>Lorem Ipsum</h2>
-          <p>Up-cycle unique pieces from unused clothing</p>
+          <h2 class="m-0">
+            Lorem Ipsum
+          </h2>
+          <p class="m-0 max-w-[12rem] text-black">
+            Up-cycle unique pieces from unused clothing
+          </p>
         </div>
       </div>
     </section>
 
-    <section class="donate">
-      <div class="donate-info">
+    <section class="bg-secondary">
+      <div class="text-white px-default-w py-default-h pb-20 leading-6 font-regular flex flex-col gap-2">
         <div>
-          <h2>WHAT WE ACCEPT</h2>
+          <h1>WHAT WE ACCEPT</h1>
           <p>
             We take in any pre-loved clothing for men, women and children, along
             with textiles.
           </p>
         </div>
         <div>
-          <h2>HOW DO I PREPARE MY CONTRIBUTIONS?</h2>
-          <ul>
+          <h1>HOW DO I PREPARE MY CONTRIBUTIONS?</h1>
+          <ul class="list-disc pl-6">
             <li>Wash and fold your items.</li>
             <li>
               If you have end of life textiles that need recycling, it helps our
@@ -57,11 +72,11 @@ import TiltedHeading from '@/components/TiltedHeading.vue'
           </ul>
         </div>
         <div>
-          <h2>WHAT HAPPENS TO YOUR ITEM?</h2>
+          <h1>WHAT HAPPENS TO YOUR ITEM?</h1>
           <p>
             First, we sort your items into:
           </p>
-          <ol>
+          <ol class="list-decimal pl-6">
             <li>
               Perfect quality - We bring them to the
               workshop to be up-cycled.
@@ -82,21 +97,24 @@ import TiltedHeading from '@/components/TiltedHeading.vue'
       </div>
     </section>
     <section>
-      <div class="location-info">
-        <div class="map">
+      <div class="flex flex-col items-center justify-center px-default-w pb-20 gap-8 tablet:flex-row ">
+        <div class="max-w-lg my-8">
           <img src="@/assets/images/donatemap.png" alt="">
         </div>
-        <div class="location">
+        <div class="mx-4">
           <div class="bubble">
             <SpeechBubble>
-              <div class="pink-bubble">
+              <div class="py-4">
                 DROP YOUR ITEMS HERE!
               </div>
             </SpeechBubble>
           </div>
-          <SpeechBubble alignment="right">
-            <div class="blue-bubble">
-              LOCATION <br>
+          <SpeechBubble
+            alignment="right"
+            class="py-4"
+          >
+            <div class="text-alt font-medium">
+              <span class="uppercase">Location</span> <br>
               Office No. 3002, Al Manara Tower <br>
               Dubai Marina <br>
               Dubai, UAE
@@ -107,97 +125,3 @@ import TiltedHeading from '@/components/TiltedHeading.vue'
     </section>
   </main>
 </template>
-<style scoped lang="scss">
-.splash-headings {
-    font-size: 4.5em;
-    display: flex;
-    color: $clr-secondary;
-    font-weight: bold;
-    // background-color: aqua;
-    justify-content: center;
-    // align-content: center;
-    padding-top: 1.15em;
-}
-
-.the {
-    // color: $clr-primary;
-    font-size: 40px;
-    // background-color: $clr-accent-1;
-    // height: min-content;
-    padding-block: 1px;
-    padding-inline: 15px;
-    border-radius: 0.2em;
-    rotate: -15deg;
-    margin-inline: 20px;
-    // transform: translateY(20px);
-}
-
-.card-section {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    text-align: center;
-    column-gap: 9em;
-    padding: 4em;
-    h2 {
-        // font-weight: bold;
-        color: $clr-secondary;
-        // text-transform: capitalize;
-    }
-
-}
-
-.info-card {
-    // background-color: aqua;
-    width: 12em;
-    // margin-inline: 40px;
-    img{
-        width: 11em;
-    }
-}
-
-.donate {
-    background-color: $clr-secondary;
-}
-
-.donate-info {
-    color: white;
-    padding: 100px;
-    padding-inline: 250px;
-
-    div {
-        padding-bottom: 50px;
-    }
-}
-
-.pink-bubble{
-    padding: 1em;
-
-    font-weight: 500;
-}
-
-.blue-bubble{
-    padding: 1em;
-    font-weight: 500;
-}
-
-.bubble{
-    padding-bottom: 2rem;
-}
-
-.map img{
-    width: 37rem;
-}
-
-.location{
-    width: 500px;
-}
-
-.location-info{
-    display: flex;
-    justify-content: center;
-    padding: 10em;
-    gap: 5em;
-    border-top: 4px dashed $clr-secondary;
-}
-</style>
