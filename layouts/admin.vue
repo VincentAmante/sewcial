d0bccda878fe632d12b75655c11c88249b7c7e62
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2023-04-27 00:29:17 +0400

    Update admin.vue
    - Removed faulty code

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,18 +1,3 @@
-<script lang="ts">
-import { NConfigProvider, GlobalThemeOverrides } from 'naive-ui'
-
-const themeOverrides: GlobalThemeOverrides = {
-  common: {
-    primaryColor: '#f00'
-  },
-  Button: {
-    textColor: '#f00'
-  },
-  Input: {
-    textColor: '#f00'
-  }
-}
-</script>
 
 <template>
   <div>
