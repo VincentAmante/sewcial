a1eee9a59a9d3b3b6a22979d559d3e79b17d9991
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2023-04-27 00:19:45 +0400

    Update AppButton.vue
    - Changed AppButton to support custom colours and text

--- components/AppButton.vue
+++ components/AppButton.vue
@@ -1,11 +1,23 @@
+<script setup lang="ts">
+defineProps({
+  bgColour: {
+    type: String,
+    default: 'secondary'
+  },
+  textColour: {
+    type: String,
+    default: 'primary'
+  }
+})
+</script>
+
 <template>
-  <button class="bg-secondary text-primary p-4 px-8 border-2 rounded-3xl outline-none max-w-[18rem]">
+  <button
+    :class="[`bg-${bgColour}`, `text-${textColour}`]"
+    class="p-4 px-8 border-2 rounded-3xl outline-none max-w-[18rem] hover:opacity-80"
+  >
     <slot>
       Button!
     </slot>
   </button>
 </template>
-
-<style scoped lang="scss">
-  // @include tailwind-overwrite;
-</style>
