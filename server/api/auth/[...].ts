8934df00d714ebb3caa0c4990ebcecbfd3be8fd9
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2023-05-07 13:39:59 +0400

    Update [...].ts

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -15,22 +15,22 @@ export default NuxtAuthHandler({
       clientId: process.env.GITHUB_CLIENT_ID || 'enter-your-client-id-here',
       clientSecret: process.env.GITHUB_CLIENT_SECRET || 'enter-your-client-secret-here' // TODO: Replace this with an env var like "process.env.GITHUB_CLIENT_SECRET". The secret should never end up in your github repository
     })
-  ],
-  callbacks: {
-    // jwt: async ({ token, user }) => {
-    //   const isSignIn = !!user
-    //   if (isSignIn) {
-    //     token.id = user ? user.id || '' : ''
-    //     token.role = user ? (user as any).role || '' : ''
-    //   }
-    //   return Promise.resolve(token)
-    // },
+  ]
+  // callbacks: {
+  //   // jwt: async ({ token, user }) => {
+  //   //   const isSignIn = !!user
+  //   //   if (isSignIn) {
+  //   //     token.id = user ? user.id || '' : ''
+  //   //     token.role = user ? (user as any).role || '' : ''
+  //   //   }
+  //   //   return Promise.resolve(token)
+  //   // },
 
-    // // Callback whenever session is checked, see https://next-auth.js.org/configuration/callbacks#session-callback
-    // session: async ({ session, token }) => {
-    //   (session as any).role = token.role;
-    //   (session as any).uid = token.id
-    //   return Promise.resolve(session)
-    // }
-  }
+  //   // // Callback whenever session is checked, see https://next-auth.js.org/configuration/callbacks#session-callback
+  //   // session: async ({ session, token }) => {
+  //   //   (session as any).role = token.role;
+  //   //   (session as any).uid = token.id
+  //   //   return Promise.resolve(session)
+  //   // }
+  // }
 })
