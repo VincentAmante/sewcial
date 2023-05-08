58367686d7dca415a9531a29d41e97edef927b47
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2023-05-09 00:12:20 +0400

    Update getLiked.post.ts

--- server/api/CatalogueItems/getLiked.post.ts
+++ server/api/CatalogueItems/getLiked.post.ts
@@ -3,7 +3,7 @@ export default defineEventHandler(async (event) => {
 
   const result = await event.context.prisma.likedCatalogueItem.findMany({
     where: {
-      userId: body.userId
+      userId: 'c336c2f7-1f04-4613-97fa-fb68777c420e'
     },
     include: {
       catalogueItem: true
