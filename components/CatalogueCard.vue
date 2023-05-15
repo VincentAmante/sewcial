a49efb8ce42c0a3d5a8c90648c7a008e81e6cb7b
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2023-05-15 23:06:20 +0400

    Update CatalogueCard.vue

--- components/CatalogueCard.vue
+++ components/CatalogueCard.vue
@@ -5,10 +5,13 @@ const props = defineProps({
     type: String,
     required: true
   },
-
   available: {
     type: Boolean,
     default: true
+  },
+  featured: {
+    type: Boolean,
+    default: false
   }
 })
 </script>
@@ -47,95 +50,3 @@ const props = defineProps({
     </div>
   </div>
 </template>
-
-<!-- Scoped Styling -->
-<!-- <style scoped lang="scss">
-.cards-container {
-    padding: .75em;
-    margin:5px;
-
-    .card {
-      color: $clr-text;
-      width: 100%; // change this depending on the thign
-      border-radius:10px;
-      transition: transform 0.5s;
-    }
-
-    // Content
-    .card-heading {
-      display:flex;
-      flex-direction: row;
-      flex-wrap: wrap;
-      justify-content: space-evenly;
-      align-items: baseline;
-      padding-top: 1rem;
-      text-align: left;
-
-      .item-name {
-        margin: 0;
-        color: $clr-secondary;
-      }
-
-      .price {
-        margin: 0 0 0 0px;
-        color: $clr-accent-1;
-      }
-    }
-    .description {
-        flex-wrap: wrap;
-        justify-content: space-evenly;
-        align-items: baseline;
-        padding: 0.5rem 1rem 1rem 1rem;
-        text-align: center;
-        margin: 0;
-    }
-
-    // Images
-    .card-image{
-      aspect-ratio: 3 / 4;
-      overflow: hidden;
-      background-size: cover;
-      background-position: center;
-      background-repeat: no-repeat;
-      border-radius: 10px 10px 10px 10px;
-      width: 100%;
-      vertical-align: middle;
-
-      transition: transform 0.5s;
-
-      &:hover {
-        cursor: pointer;
-        transform: scale(1.1);
-        -webkit-transform: scale(1.1);
-        .card-image {
-          opacity: 1;
-        }
-      }
-
-      &.available{
-        .sold-text {
-          display: none;
-        }
-      }
-
-      &.notavailable {
-        .sold-text {
-          display: inline-block;
-          position: relative;
-          height: 100%;
-          width: 100%;
-          box-shadow: inset 0 0 0 2000px rgba(255, 78, 78, 0.5);
-        }
-
-        h1 {
-          position: absolute;
-          left: 50%;
-          top: 50%;
-          transform: translate(-50%, -50%);
-          color: #fff;
-        }
-      }
-
-    }
-  }
-</style> -->
