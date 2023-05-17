44b69d2772fffc02021c4a96c15cb66a73c3f08b
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2023-05-17 18:07:52 +0400

    Update sign-in.vue

--- pages/sign-in.vue
+++ pages/sign-in.vue
@@ -4,8 +4,25 @@ definePageMeta({
     unauthenticatedOnly: true
   }
 })
+const { signIn } = useAuth()
+
 </script>
 
 <template>
-  <div />
+  <main class="bg-secondary overflow-hidden flex flex-col items-center min-h-screen">
+    <div class="w-full flex flex-col items-center py-20 text-primary gap-8">
+      <h1 class="text-4xl">
+        Sign In
+      </h1>
+      <button
+        class="flex justify-center text-3xl bg-accent-1 gap-4 py-4 px-2 rounded-md text-primary hover:bg-dark hover:text-accent-1 w-full max-w-xs"
+        @click="signIn('github')"
+      >
+        <div>
+          <AppIcon :icon="['fab', 'github']" />
+        </div>
+        <div>Github</div>
+      </button>
+    </div>
+  </main>
 </template>
