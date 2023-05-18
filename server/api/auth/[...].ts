94da9777c08ece58c7bc51621ac1bc5c3c3be2ce
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2023-05-18 12:38:14 +0400

    Assigned custom sign in

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -2,9 +2,11 @@ import GithubProvider from 'next-auth/providers/github'
 import { NuxtAuthHandler } from '#auth'
 
 export default NuxtAuthHandler({
-  // TODO: SET A STRONG SECRET, SEE https://sidebase.io/nuxt-auth/configuration/nuxt-auth-handler#secret
+  pages: {
+    signIn: '/sign-in'
+  }
+
   secret: process.env.AUTH_SECRET,
-  // TODO: ADD YOUR OWN AUTHENTICATION PROVIDER HERE, READ THE DOCS FOR MORE: https://sidebase.io/nuxt-auth
   providers: [
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     GithubProvider.default({
