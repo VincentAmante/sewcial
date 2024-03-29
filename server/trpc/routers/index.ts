b96150fb3d1db8bb7eb7f0a8dfd7c3e5949d7e7b
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2024-03-29 23:44:22 +0400

    Added catch to instagram fetch

--- server/trpc/routers/index.ts
+++ server/trpc/routers/index.ts
@@ -1,18 +1,14 @@
-import { z } from 'zod'
 import { publicProcedure, router } from '../trpc'
 
 export const appRouter = router({
-  hello: publicProcedure
-    .input(
-      z.object({
-        text: z.string().nullish()
+  fetchInstagram: publicProcedure
+    .query(async () => {
+      const fields = ['id', 'caption', 'thumbnail_url', 'permalink', 'media_url'].join(',')
+      const url = `https://graph.instagram.com/me/media?fields=${fields}&access_token=${process.env.INSTAGRAM_ACCESS_TOKEN}`
+      return await $fetch(url).catch((err) => {
+        console.error(err)
+        return []
       })
-    )
-    .query(({ input }) => {
-      return {
-        greeting: `hello ${input?.text ?? 'world'}`,
-        time: new Date()
-      }
     })
 })
 
